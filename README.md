c14b38d1c2f5512a1357c486d5301da7e5811a85
MetaFight <MetaFight@users.noreply.github.com>
2018-03-26 15:00:35 +0000

    Travis #10

---
+++ README.md
@@ -0,0 +1,18 @@
+# EOF Hackspace Website Repository
+
+This is the repository containing the EOF website source.
+
+## Branches
+
+* `master` - This is where the raw site code lives.
+* `gh-pages` - This is where the *compiled* site code lives.  This branch is updated automatically so you don't need to touch it.
+
+## Development
+
+Development happens in the `master` branch.  Changes to this branch are automatically built using Travis CI and then pushed to the `gh-pages` branch.  Once there, the changes can be viewed at https://eof-hackspace.github.io/website-src/ .  This should allow the developer to preview their changes (relatively) quickly.
+
+When the changes in `gh-pages` are ready to go live, the developer should notify me, Yves Conan.  I'll trigger a Production build.
+
+The Production build is very similar.  It takes the contents of `master`, build them, and deploys them to the `master` branch of the `EOF-Hackspace/website-deployed` repository.  The changes will then become visible at https://www.eof.org.uk .
+
+I'm hoping to simplify the build/deployment to Production in the future.
---
+++ scripts/ci-build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# skip if build is triggered by pull request
+if [ $TRAVIS_PULL_REQUEST == "true" ]; then
+  echo "this is PR, exiting"
+  exit 0
+fi
+
+TARGET_REPO="website-src"
+TARGET_BRANCH="gh-pages"
+USE_PROD_CONFIG="false"
+
+if [ $IS_PROD_BUILD == "true" ]; then
+  TARGET_REPO="website-deployed"
+  TARGET_BRANCH="master"
+  # temporarily disabled until PROD domain is sorted out.
+  #USE_PROD_CONFIG="true"
+fi
+
+# enable error reporting to the console
+set -e
+
+# cleanup "_site"
+rm -rf _site
+mkdir _site
+
+# clone target repo to "_site"
+git clone https://${GH_TOKEN}@github.com/EOF-Hackspace/${TARGET_REPO}.git --branch ${TARGET_BRANCH} _site
+
+if [ $USE_PROD_CONFIG == "true" ]; then
+  mv -f ./_config.production.yml ./_config.yml
+fi
+
+# build with Jekyll into "_site"
+bundle exec jekyll build
+#bundle exec htmlproofer ./_site
+
+if [ $DISABLE_ROBOTS == "true" ]; then
+  mv -f ./robots.disabled.txt ./_site/robot.txt
+fi
+
+# push
+cd _site
+git config user.email "MetaFight@users.noreply.github.com"
+git config user.name "MetaFight"
+git add --all
+git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
+git push --force origin ${TARGET_BRANCH}
