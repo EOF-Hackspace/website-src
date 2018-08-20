33c25a439ec2c4fc8baefe7d9094f3054b478edd
MetaFight <yarghmatey@hotmail.com>
2018-08-21 00:22:01 +0100

    Misc dev environment improvements.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec jekyll serve -H 0.0.0.0 --livereload --livereload-port 35729 --force_polling
+exec bundle exec jekyll serve -H 0.0.0.0 --livereload --livereload-port 35729 --force_polling
