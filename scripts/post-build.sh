54fe95d027b8e25eaff333b6497dfc4cfa73e45a
Yves Conan <yarghmatey@hotmail.com>
2023-10-01 17:13:59 +0100

    debugging

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 cd ./src
+ls
+ls ./_site/
 
 if [[ $GITHUB_REF_NAME != "production" ]]; then
   echo "Using disabled robots.txt."
   cp -f ./robots.disabled.txt ./_site/robots.txt
 fi
 
-if [[ $GITHUB_REF_NAME != "production" ]]; then
+if [[ $GITHUB_REF_NAME == "production" ]]; then
   rm -f ./_site/CNAME*
   echo "Using Production CNAME."
   echo "eof.org.uk" > ./_site/CNAME
